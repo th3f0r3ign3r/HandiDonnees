892926c93c56f5a9cdc3c3ac9fcf1a7522f0caef
Ronel <ronel.kpossou@epitech.eu>
2024-03-15 20:45:35 +0100

    first MVP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,226 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import {
+  ArrowRightIcon,
+  GiftTopIcon,
+  HandRaisedIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HeartIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Header />
+
+      <div className="relative">
+        <div className="mx-auto max-w-7xl">
+          <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+            <svg
+              className="absolute inset-y-0 right-8 hidden h-screen w-80 translate-x-1/2 transform fill-white lg:block"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="0,0 180,0 50,100 0,100" />
+            </svg>
+
+            <div className="relative px-6 py-32 sm:py-40 lg:px-0 lg:py-[16.6rem] lg:pr-0">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-[5rem] leading-[.9]">
+                  Célébrons la diversité. <br /> Unis pour l&apos;égalité.
+                </h1>
+                <p className="mt-6 text-lg leading-8 tracking-tight text-gray-600">
+                  Unis par la diversité, guidés par l&apos;inclusion - bienvenue
+                  dans notre communauté dédiée à l&apos;émancipation des
+                  personnes handicapées.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <Link
+                    href="#"
+                    className="inline-flex items-center gap-3 px-10 py-5 rounded-full bg-pink-200/80 animate-bounce hover:animate-none duration-300 ease-in-out"
+                  >
+                    <HeartIcon className="h-8 w-8 text-red-500" />
+                    <span className="text-lg tracking-tight font-semibold">
+                      Je veux faire un don
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <Image
+            className="aspect-[3/2] object-cover lg:aspect-auto lg:h-screen lg:w-full"
+            src="/images/handicap.png"
+            width={3216}
+            height={2136}
+            alt=""
+          />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <main className="flex flex-col max-w-7xl mx-auto px-6 lg:px-8 my-36 gap-36">
+        <section>
+          <h2 className="tracking-tighter font-bold text-blue-600 text-4xl">
+            Tout le monde a un rôle à jouer.
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p className="mt-2 tracking-tight text-gray-500">
+            Ensemble, nous pouvons faire une différence. <br /> Découvrez
+            comment vous pouvez contribuer à notre cause.
           </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+            <div className="p-3 px-6 rounded-full border inline-flex items-center gap-5 shadow overflow-hidden hover:scale-105 duration-300">
+              <HandRaisedIcon className="h-10 w-10 text-blue-400" />
+              <div>
+                <h3 className="text-2xl font-semibold tracking-tighter">
+                  Bénévolat
+                </h3>
+                <p className="leading-5 tracking-tight text-sm text-gray-600 mt-2">
+                  Bénévoles, étudiants, enseignants, salariés, entreprises,
+                  sportifs, écoles. <br /> Vous avez toutes et tous le pouvoir
+                  d&apos;agir.
+                </p>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <div className="p-3 px-6 rounded-full border inline-flex items-center gap-5 shadow overflow-hidden hover:scale-105 duration-300">
+              <GiftTopIcon className="h-10 w-10 text-blue-400" />
+              <div>
+                <h3 className="text-2xl font-semibold tracking-tighter">
+                  Aides Sociales
+                </h3>
+                <p className="leading-5 tracking-tight text-sm text-gray-600 mt-2">
+                  Faites un don pour soutenir les personnes en situation de
+                  handicap. <br /> Votre aide est précieuse.
+                </p>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            <div className="p-3 px-6 rounded-full border inline-flex items-center gap-5 shadow overflow-hidden hover:scale-105 duration-300">
+              <QuestionMarkCircleIcon className="h-10 w-10 text-blue-400" />
+              <div>
+                <h3 className="text-2xl font-semibold tracking-tighter">
+                  Autres Actions
+                </h3>
+                <p className="leading-5 tracking-tight text-sm text-gray-600 mt-2">
+                  Vous avez une idée, une suggestion ou une proposition ? <br />
+                  Partagez-la avec nous.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className="tracking-tighter font-bold text-blue-600 text-4xl">
+            L&apos;actualité <br /> de la communauté.
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="mt-2 tracking-tight text-gray-500">
+            Découvrez les dernières actualités, les projets en cours et les
+            événements à venir.
           </p>
-        </a>
-      </div>
-    </main>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+            <div className="flex flex-col items-center justify-between gap-5 border p-3 rounded-[2.3rem]">
+              <img
+                src="https://www.handicap-international.fr/sn_uploads/federation/country/BENIN.jpg?maxw=1110&maxh=1110"
+                alt="Jeune garçon pouvant désormais marcher grâce aux séances de réadaptation à base communautaire dont il bénéficie. Il peut se rendre à l’école et est intégré en cursus scolaire ordinaire."
+                className="w-full h-52 object-cover rounded-[1.5rem]"
+              />
+              <div>
+                <span className="text-base tracking-tighter mt-4 underline text-gray-400">
+                  Handicap International
+                </span>
+                <h3
+                  className="text-xl font-semibold tracking-tighter mt-3 line-clamp-3"
+                  title="Au Bénin, HI œuvre à l’amélioration de la qualité de vie des personnes handicapées et favorise leur pleine participation à la vie économique et sociale du pays."
+                >
+                  Au Bénin, HI œuvre à l&apos;amélioration de la qualité de vie
+                  des personnes handicapées et favorise leur pleine
+                  participation à la vie économique et sociale du pays.
+                </h3>
+                <Link
+                  href="/news/1"
+                  className="w-full rounded-full py-3 inline-block mt-5 font-semibold bg-blue-100 text-center"
+                >
+                  Lire la suite
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <Link
+            href="/news"
+            className="mt-10 block px-5 py-3 bg-blue-500 w-max rounded-full text-white tracking-tight font-semibold"
+          >
+            Tout voir
+            <ArrowRightIcon className="h-5 w-5 inline-block ml-2" />
+          </Link>
+        </section>
+
+        <section>
+          <div className="mx-auto max-w-3xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tighter text-gray-900 sm:text-4xl">
+              Grâce à votre soutien, <br /> nous avons pu réaliser de grandes choses.
+            </h2>
+            <p className="mt-3 text-base leading-7 text-gray-600">
+              Nous sommes fiers de ce que nous avons accompli ensemble. Voici
+              quelques chiffres clés.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+              <p className="flex-none text-3xl font-bold tracking-tighter text-gray-900">
+                750.000+
+              </p>
+              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                <p className="text-lg font-semibold tracking-tight text-gray-900">
+                  Personnes aidées
+                </p>
+                <p className="mt-2 text-base leading-6 text-gray-600 tracking-tighter">
+                  Depuis notre création, jusqu&apos;à aujourd&apos;hui, nous avons
+                  aidé toutes ces personnes en situation de handicap.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-40">
+              <p className="flex-none text-3xl font-bold tracking-tighter text-white">
+                250 Millions <br /> FCFA
+              </p>
+              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                <p className="text-lg font-semibold tracking-tight text-white">
+                  Fonds collectés
+                </p>
+                <p className="mt-2 text-base leading-6 tracking-tighter text-gray-400">
+                  Grâce à vos dons, nous avons pu collecter cette somme pour
+                  financer nos actions.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+              <p className="flex-none text-3xl font-bold tracking-tighter text-white">
+                10.000+
+              </p>
+              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                <p className="text-lg font-semibold tracking-tight text-white">
+                  Bénévoles engagés
+                </p>
+                <p className="mt-2 text-base leading-6 tracking-tighter text-indigo-200">
+                  Grâce à l&apos;engagement de ces bénévoles, nous avons pu
+                  réaliser de grandes choses. Merci à eux.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </>
   );
 }
---
+++ config/index.ts
@@ -0,0 +1,15 @@
+export default {
+  app: {
+    title: "Handidonnées Bénin",
+    description:
+      "Plateforme de dons, bénévolat et de actions sociales recensant les besoins des personnes en situation d'handicap au Bénin",
+  },
+  navigations: [
+    { name: "Accueil", href: "#" },
+    { name: "Dons", href: "#" },
+    { name: "Bénévolat", href: "#" },
+    { name: "Offres d'emploi", href: "#" },
+    { name: "Actualités", href: "#" },
+    { name: "Contactez-Nous", href: "/about" },
+  ],
+} as const;
