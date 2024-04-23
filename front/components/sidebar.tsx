7f5e929d532ed0cd880a97fffdc0834b42c3ad9c
Netsuretsu <constantin-joval.awanchede@epitech.eu>
2024-04-23 20:24:07 +0100

    added user dashboard page

---
+++ front/components/sidebar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import NavLinks from './navLinks';
+
+export default function SideBar() {
+    return (
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link
+                className="mb-2 flex h-fit items-end justify-start rounded-md bg-blue-600 p-4"
+                href="/"
+            >
+                <div className="w-32 text-white font-bold md:w-40">
+                    HANDIDONNEES
+                </div>
+            </Link>
+            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+                <NavLinks />
+                <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+                <form>
+                    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                        {/* <PowerIcon className="w-6" /> */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                        </svg>
+
+                        <div className="hidden md:block">Déconnexion</div>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
